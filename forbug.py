420a92186f3e16179194b8bdd55d026c9bdf4b48
dawdameet <dawdameet6338@gmail.com>
2025-11-06 23:21:40 +0530

    Fix bug #2: Changed comparison operator from > to >= in tax calculation

--- forbug.py
+++ forbug.py
@@ -4,9 +4,9 @@
 for i in range(0, 10):
     print(i)
 
-# Bug 2: Wrong comparison operator (should be >= not >)
+# Bug 2: Fixed - Changed to >= operator
 def calculate_tax(income):
-    if income > 50000:  # Bug: should be >=
+    if income >= 50000:  # Fixed: changed from > to >=
         return income * 0.3
     else:
         return income * 0.2
